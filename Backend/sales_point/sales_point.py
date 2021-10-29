ab20895bdf4d4287fe5ec10ffaa259d8c1352ea5
l0gesh29 <logeshperiyasamy24@gmail.com>
2021-10-29 12:55:37 +0530

    Added files

---
+++ Backend/sales_point/sales_point.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, logesh and contributors
+# For license information, please see license.txt
+
+#import frappe
+from frappe.model.document import Document
+
+class SalesPoint(Document):
+	pass
